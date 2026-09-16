0079e8df7f3278a27d5b3d1f5986d4653989f693
MiielsGH <athoil.kamil@gmail.com>
2026-09-16 15:04:16 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 int nilaiSiswa;
 
 Console.WriteLine("Aplikasi Nilai Siswa")
+
+Console.WriteLine("Masukkan nama siswa: ");
+namaSiswa = Console.ReadLine()
+
+Console.WriteLine("Masukkan nilai siswa: ");
+nilaiSiswa = Console.ReadLine()
