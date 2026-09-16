efbc3e90566724a16207a9f63cceca92a9d232b3
MiielsGH <athoil.kamil@gmail.com>
2026-09-16 15:07:45 +0700

    Logika Status

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿string namaSiswa;
 int nilaiSiswa;
 
-Console.WriteLine("Aplikasi Nilai Siswa")
+Console.WriteLine("Aplikasi Nilai Siswa");
 
 Console.WriteLine("Masukkan nama siswa: ");
-namaSiswa = Console.ReadLine()
+namaSiswa = Console.ReadLine();
 
 Console.WriteLine("Masukkan nilai siswa: ");
-nilaiSiswa = Console.ReadLine()
+nilaiSiswa = Console.ReadLine();
+
+if (nilaiSiswa >= 75)
+{
+    Console.WriteLine($"Selamat {namaSiswa}, Anda lulus dengan nilai {nilaiSiswa}");
+}
+else
+{
+    Console.WriteLine($"Maaf kepada {namaSiswa}, Anda tidak lulus dengan nilai {nilaiSiswa}");
+}
